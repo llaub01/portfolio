43ae527409309cc2bbe18b502bfdbeb8ea112805
Lonnie Laub <llaub01@gmail.com>
2018-09-17 21:13:52 -0500

    portfolio framework - needs content

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+import resumeData from './components/resume/resumeData';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/resume/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+  "imagebaseurl":"http://localhost:3000/",
+  "name": "Lonnie Laub",
+  "role": "new, eager to learn developer"
+  }
+export default resumeData
