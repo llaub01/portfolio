e48b4487194a2119c7693410e9862b0f8f226f99
Lonnie Laub <llaub@Allcovered.com>
2018-09-25 23:58:38 -0500

    content work

--- src/portfolioData.js
+++ src/portfolioData.js
@@ -26,37 +26,37 @@ let portfolioData = {
 		"name": "University of Kansas",
 		"degree": "Full Stack Web Development Certificate",
 		"grad": "March 2018",
-		"description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+		"description": "*Immersive 24 week course to learn full stack software development<br>*HTML, CSS, Javascript, MongoDB, MySQL, Express.js, React, Node.js<br>",
 	},
 	"education2": {
 		"name": "Kansas State University",
 		"degree": "BS in Social Sciences",
 		"grad": "May 2004",
-		"description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+		"description": "*Emphasis in Human-Environmental Interaction<br>*Electrical Engineering & Computer Science including Basic, C++ and Java courses taken<br>",
 	},
 	"education3": {
 		"name": "Johnson County Community College",
 		"degree": "Associate of Arts",
 		"grad": "May 2002",
-		"description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+		"description": "*Emphasis in Microcomputer Hardware & Network Technologies<br>*Windows 2000 Professional, Windows 2000 Server, Linux, LAN Cabling, Adobe Photoshop and Illustrator<br>",
 	},
 	"work1": {
 		"name": "All Covered",
 		"title": "Service Delivery Manager",
 		"date": "June 2018 - Present",
-		"description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+		"description": "*Managed a distributed team of Systems Engineers from Kansas City, St. Louis and Des Moines<br>*Managed a  book of business of 70+ Managed IT Services customers throughout the midwest<br>*Ensured customer needs were met as to subscribed Managed IT Services<br>*Assisted with technical sales and ensured customer satisfaction<br>",
 	},
 	"work2": {
 		"name": "Homebase",
 		"title": "VP of Customer Success",
 		"date": "December 2017 - March 2018",
-		"description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+		"description": "*One of the first 10 employees in the company<br>*Tasked with building a comprehensive customer support and sales support team<br>*Assisted with company website work in Wordpress as needed<br>*Collected and published user stories for customer feature requests and bug fixes<br>*Conducted detailed product demonstrations for prospective customers and ongoing training for current customers<br>*Developed markets by connecting and building relationships with other movers and shakers in the industry<br>*Worked on the team responsible for building SOP and operations for the company<br>*Developed and acted in multimedia instructional content for users of all levels<br>",
 	},
 	"work3": {
 		"name": "Suitable Technologies",
 		"title": "Senior Manager of Customer Success",
 		"date": "October 2013 - November 2017",
-		"description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+		"description": "*First and highest ranking employee in the Kansas City area<br>*Spearheaded project of building a strong technical support team from a team of 1 to 7 individuals<br>*Managed all operations of Kansas City office, including staffing, training, employee development, office logistics, physical office build-out and move, etc.<br>*Developed, managed, delegated, and maintained all aspects of company’s technical support and customer service<br>*Conducted product demonstrations, technical sales  and training for individuals, start-up’s, C-level executives, and celebrities<br>*Engineered complex solutions for a large range of problems, including robotics devices, network and WiFi, and security reviews<br>*Wrote and maintained all product documentation and online product usage content<br>Developed a market in Kansas City through consistent business development, unique events, trade shows, and more<br>*Assisted with HTML, CSS and Jekyll work for product documentaion and knowledgebase sites<br>",
 	},
 
 
